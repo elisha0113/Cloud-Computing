244708d685b306b6d9be6ed5aca54bea36af16c3
elisha0113 <160813317+elisha0113@users.noreply.github.com>
2024-09-24 13:22:43 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,11 @@ $db_username = 'root';
 $db_password = '1234'; // Use the password you set
 $db_name = 'CONVO';
 $db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+$conn = new mysqli("localhost", "root", "1234", "CONVO");
+
 
 // Check for connection errors
-if ($mysqli->connect_error) {
+if ($conn->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 ?>
