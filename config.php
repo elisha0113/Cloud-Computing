22946c0eaa8e721166e4f2c76c3adf3a8f11b6ae
elisha0113 <160813317+elisha0113@users.noreply.github.com>
2024-09-24 11:29:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $currency = '₹';
 $db_username = 'root';
 $db_password = '1234'; // Use the password you set
-$db_name = 'bolt';
+$db_name = 'CONVO';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
 
